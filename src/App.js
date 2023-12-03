f5be5bc756cdddf23bfe77f30786deb029302a65
Zman82-dot <109772315+Zman82-dot@users.noreply.github.com>
2023-12-02 23:06:53 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import { AppProvider } from './context/AppContext';
 import Budget from './components/Budget';
 import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
+import Market20trends from './Market20trends.webp';
 import AllocationForm from './components/AllocationForm';
 import RemainingBudget from './components/Remaining';
+import CurrencyDropdown from './components/CurrencyDropdown';
+
 
 const App = () => {
     return (
-        <AppProvider>
-            <div className='container'>
+        <AppProvider style={{backgroundImage:'linear-gradient(white,black)'}}>
+            <div className='card' style={{backgroundImage:'url("https://www.passionateinmarketing.com/wp-content/uploads/2020/08/data-original-3.jpg")',color:"white"}}>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                 <div className='row mt-3'>
                     <div className='col-sm'>
@@ -24,20 +27,28 @@ const App = () => {
                         <ExpenseTotal />
                     </div>
                 </div>
+                <div className='mt-3'>Currency
+                            <CurrencyDropdown />
+                        </div>
                 <h3 className='mt-3'>Allocation</h3>
                 <div className='row '>
                     <div className='col-sm'>
                         <ExpenseList />
                     </div>
                 </div>
+
                 <h3 className='mt-3'>Change allocation</h3>
                 <div className='row mt-3'>
                     <div className='col-sm'>
                         <AllocationForm/>
                     </div>
+
+
+
                 </div>
             </div>
         </AppProvider>
+
     );
 };
 
